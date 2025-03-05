9a65ee8f4dbe743e29e3f2073cf8bed598ab3393
Alina Yildir <ms.alina.yildir@gmail.com>
2025-03-05 01:12:44 -0700

    User Inputs

--- mortgage_payment_calculator.py
+++ mortgage_payment_calculator.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 import math
 
 st.title("Mortgage Payment Calculator")
+st.write("### Enter Loan Details")
+
+col1, col2 = st.columns(2)
+
+home_price = col1.number_input("Home Price ($)", min_value=0, value=0)
+down_payment = col1.number_input("Down Payment ($)", min_value=0, value=0)
+annual_interest_rate = col2.number_input("Annual Interest Rate (%)", min_value=0.0, value=0.0)
+loan_duration_years = col2.number_input("Loan Term (Years)", min_value=1, value=1)
